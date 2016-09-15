bb5ef337e4b4d1f2a05dd96f2d09080c46c8205a
BobrImperator <lordboberro@gmail.com>
2016-09-15 16:49:02 +0200

    almost working version

--- alive_cells.rb
+++ alive_cells.rb
@@ -22,50 +22,63 @@ class TranslateToMatrix < Struct.new(:grid_as_numbers)
 end
 
 class NextGeneration < Struct.new(:matrix)
+    def next
+      NextGeneration.new(next_generation)
+    end
+
+    def to_s
+      matrix.map { |line| line.join('') + "\n" }.join()
+    end
+
     def next_generation
         matrix.map.with_index do |row, y|
             row.map.with_index { |cell, x| dead_or_alive(cell, [x, y]) }
         end
   end
 
-  def matrix_range
-    0..(matrix.size-1)
-  end
+    def matrix_range
+        0..(matrix.size - 1)
+    end
 
     def dead_or_alive(cell, coordinates)
-      if cell == 1
+        if cell == 1
+            case  live_neighbours(coordinates)
+            when  0..1 then 0
+            when 2..3 then 1
+            else 0
+            end
 
-      # Any live cell with fewer than two live neighbors dies, as if caused by under-population.
-      # Any live cell with two or three live neighbors lives on to the next generation.
-      # Any live cell with more than three live neighbors dies, as if by overcrowding.
-      else
-        binding.pry if coordinates == [3,2]
-        if neighbours(*coordinates).select { |cell| cell == 1 }.size >= 3
-          1
         else
-          0
+            # binding.pry if coordinates == [3,2]
+            if live_neighbours(coordinates) >= 3
+                1
+            else
+                0
+            end
+
         end
-      # Any dead cell with exactly three live neighbors becomes a live cell, a if by reproduction.
-      end
+    end
+
+    def live_neighbours(coordinates)
+        neighbours(*coordinates).select { |cell| cell == 1 }.size
     end
 
     def neighbours(x, y)
-      neighbours_coordinates(x,y).map { |nx, ny| matrix[ny][nx] }
+        neighbours_coordinates(x, y).map { |nx, ny| matrix[ny][nx] }
     end
 
     def neighbours_coordinates(x, y)
-      eight_directions.map { |vx, vy| [x+vx, y+vy] }.select { |nx, ny| matrix_range.include?(nx) && matrix_range.include?(ny) }
+        eight_directions.map { |vx, vy| [x + vx, y + vy] }.select { |nx, ny| matrix_range.include?(nx) && matrix_range.include?(ny) }
     end
 
     def eight_directions
-      [
-        [-1, -1], [0,-1],[1,-1],
-        [-1, 0],         [1, 0],
-        [-1, 1], [0, 1], [1, 1]
-     ]
+        [
+            [-1, -1], [0, -1], [1, -1],
+            [-1, 0],         [1, 0],
+            [-1, 1], [0, 1], [1, 1]
+        ]
     end
 end
-
 
 input = '.........*
 .*.*...*..
@@ -82,7 +95,8 @@ input = '.........*
 input_translate_test = TranslateToMatrix.new(input)
 input_translate_test.matrix
 next_generation = NextGeneration.new(input_translate_test.matrix)
-print next_generation.next_generation
+1.times { next_generation = next_generation.next }
+puts next_generation
 
 output = '..........
 ...*......
