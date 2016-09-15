3b57050ed48af5e499acb4506dd4a6a9bdaac8d1
BobrImperator <lordboberro@gmail.com>
2016-09-15 17:06:53 +0200

    refactoring specs, adding contexts

--- spec/alive_cells_spec.rb
+++ spec/alive_cells_spec.rb
@@ -2,7 +2,27 @@ require 'spec_helper'
 load 'lib/alive_cells.rb'
 
 describe NextGeneration do
-  it 'should work' do
-    expect(0).to eq(1)
-  end
+    context 'any live cell with less then 2 neighbours should be dead' do
+        let(:generator) { NextGeneration.new(start_matrix) }
+
+        context '0 live neighbours' do
+            let(:start_matrix) do
+                [
+                    [0, 0, 0],
+                    [0, 1, 0],
+                    [0, 0, 0]
+                ]
+            end
+
+            let(:end_matrix) do
+              [
+                  [0, 0, 0],
+                  [0, 0, 0],
+                  [0, 0, 0]
+              ]
+            end
+
+            it { expect(generator.next.matrix).to eq(end_matrix) }
+        end
+    end
 end
